bc18e095cb1a9090a586efa2e9a4a70b9121e524
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-29 18:10:45 -0500

    create the draw function that draw the lines of the grid making cells thath are the same size of the RESOLUTION constant

--- js/script.js
+++ js/script.js
@@ -4,15 +4,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 600;
 
-const RESOLUTION = canvas.height * 0.1;
+const RESOLUTION = 15; //cell size
 const COLS = canvas.width / RESOLUTION;
 const ROWS = canvas.height / RESOLUTION;
 
 function createGrid() {
-  const mainGrid = new Array(COLS)
-    .fill(null)
-    .map(() => new Array(ROWS).fill(0));
-  return mainGrid;
+  const grid = new Array(COLS).fill(null).map(() => new Array(ROWS).fill(0));
+  return grid;
 }
 
-console.log(createGrid());
+function drawGrid(grid) {
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      const cell = grid[col][row];
+      ctx.beginPath();
+      ctx.rect(col * RESOLUTION, row * RESOLUTION, RESOLUTION, RESOLUTION);
+      ctx.stroke();
+    }
+  }
+}
+
+const mainGrid = createGrid();
+drawGrid(mainGrid);

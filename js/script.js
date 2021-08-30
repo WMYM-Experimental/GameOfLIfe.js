0aee7c99ac082351cf3f819fa82fcae2ec3eb0c3
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-29 20:38:31 -0500

    made life function that contains all the rules of game of life, also i added a animate function for animate pourposes, and finally i added some comments to make the code more understandable

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
+//variables and constants
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
 canvas.height = 600;
 
-const RESOLUTION = 15; //cell size
+const RESOLUTION = 5; //cell size
 const COLS = canvas.width / RESOLUTION;
 const ROWS = canvas.height / RESOLUTION;
 
 function getRandomInt(min, max) {
   let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+
   return randomInt;
 }
 
+//setup
 function createGrid() {
   const grid = new Array(COLS)
     .fill(null)
     .map(() => new Array(ROWS).fill(null).map(() => getRandomInt(0, 1)));
+
   return grid;
 }
 
@@ -24,13 +28,66 @@ function drawGrid(grid) {
   for (let col = 0; col < grid.length; col++) {
     for (let row = 0; row < grid[col].length; row++) {
       const cell = grid[col][row];
+
       ctx.beginPath();
       ctx.rect(col * RESOLUTION, row * RESOLUTION, RESOLUTION, RESOLUTION);
-      ctx.stroke();
+
+      ctx.fillStyle = cell ? "#444" : "#eee"; //if is alive is black if is not cell is white
+
+      ctx.fill();
     }
   }
 }
 
-const mainGrid = createGrid();
-drawGrid(mainGrid);
-console.log(mainGrid);
+//rules
+function life(grid) {
+  const nextGen = grid.map((array) => [...array]); // 'loop' each array remmeber mainGrid is a matrix (an array of arrays)
+
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      const cell = grid[col][row];
+      let numNeighbours = 0;
+
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //3 by 3 grid
+          if (i === 0 && j === 0) {
+            continue; //for not detectting itself
+          }
+
+          //handle 'cell collition with canvas border'
+          const xCell = col + i;
+          const yCell = row + j;
+
+          //if cell is not in a collition with canvas border work
+          if (xCell >= 0 && yCell >= 0 && xCell < COLS && yCell < ROWS) {
+            const neighbour = grid[col + i][row + j];
+            numNeighbours += neighbour;
+          }
+        }
+      }
+
+      //rules
+      if (cell === 1 && numNeighbours < 2) {
+        nextGen[col][row] = 0;
+      } else if (cell === 1 && numNeighbours > 3) {
+        nextGen[col][row] = 0;
+      } else if (cell === 0 && numNeighbours === 3) {
+        nextGen[col][row] = 1;
+      }
+    }
+  }
+  return nextGen;
+}
+
+function animate() {
+  mainGrid = life(mainGrid); //
+  drawGrid(mainGrid);
+  requestAnimationFrame(animate);
+}
+
+//listener
+
+//run
+let mainGrid = createGrid();
+animate();
